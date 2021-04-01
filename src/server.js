e98230c7c0225de491ad9e272212ef514817f877
VitorHugoAntunes <vitor.passos3@etec.sp.gov.br>
2021-04-01 00:45:52 -0300

    Frontend, Node server and EJS template engine

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const server = express();
+const routes = require("./routes")
+
+// Usando template engine
+server.set('view engine', 'ejs')
+
+// Habilitando arquivos estaticos
+server.use(express.static("public"))
+
+server.use(routes)
+
+server.listen(3000, () => console.log("rodando"));
