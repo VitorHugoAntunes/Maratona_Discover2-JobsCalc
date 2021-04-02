51a40e63358b15c85b69db2a836f041b7fdad839
VitorHugoAntunes <vitor.passos3@etec.sp.gov.br>
2021-04-02 18:58:30 -0300

    Jobs data structure

--- src/routes.js
+++ src/routes.js
@@ -12,9 +12,38 @@ const profile = {
     "vacation-per-year": 4
 }
 
-routes.get('/', (req, res) => res.render(views + "index"));
+const jobs = [
+    {
+        id: 1,
+        name: "Pizzaria Guloso",
+        "daily-hours": 2,
+        "total-hours": 60,
+        created_at: Date.now()
+    },
+    {
+        id: 2,
+        name: "OneTwo Project",
+        "daily-hours": 3,
+        "total-hours": 47,
+        created_at: Date.now()
+    },
+]
+
+routes.get('/', (req, res) => res.render(views + "index", { jobs }));
 
 routes.get('/job', (req, res) => res.render(views + "job"));
+routes.post('/job', (req, res) => {
+    const lastId = jobs[jobs.length - 1]?.id || 1; // Verificando se ha algum elemento no array, caso nao, atribui id 1 ao primeiro elemento
+     
+    jobs.push({
+        id: lastId + 1, // atribuindo + 1 no id dos proximos elementos
+        name: req.body.name,
+        "daily-hours": req.body["daily-hours"],
+        "total-hours": req.body["total-hours"],
+        created_at: Date.now() // Atribuindo a data de quando o job foi criado
+    })
+    return res.redirect('/')
+});
 
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"));
 
