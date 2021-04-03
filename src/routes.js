6a9d26e8f917b7067e7adb28f4018f69b44bb462
VitorHugoAntunes <vitor.passos3@etec.sp.gov.br>
2021-04-03 15:17:06 -0300

    Calculation to update hourly cost

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,50 @@ const routes = express.Router()
 
 const views = __dirname + "/views/"
 
-const profile = {
-    name: "Vitor Hugo",
-    avatar: "https://avatars.githubusercontent.com/u/51717305?v=4",
-    "monthly-budget": 3000,
-    "days-per-week": 5,
-    "hours-per-day": 5,
-    "vacation-per-year": 4,
-    "value-hour": 75,
+const Profile = {
+    data: {
+        name: "Vitor Hugo",
+        avatar: "https://avatars.githubusercontent.com/u/51717305?v=4",
+        "monthly-budget": 3000,
+        "days-per-week": 5,
+        "hours-per-day": 5,
+        "vacation-per-year": 4,
+        "value-hour": 75,
+    },
+    
+    controllers: {
+        index(req, res){
+            res.render(views + "profile", { profile: Profile.data })
+        },
+
+        update(req, res) {
+            // req.body para pegar os dados
+            const data = req.body
+            
+            // Definir quantas semanas tem em um ano
+            const weeksPerYear = 52
+
+            // Remover as semanas de ferias do ano, para pegar quantas semanas tem em um mes
+            const weeksPerMonth = (weeksPerYear - data["vacation-per-year"]) / 12
+
+            // total de horas trabalhadas na semanas
+            const weekTotalHours = data["hours-per-day"] * data["days-per-week"]
+
+            // horas trabalhadas no mes
+            const monthlyTotalHours = weekTotalHours * weeksPerMonth
+
+            // Qual sera o valor da minha horas
+            const valueHour =  data["monthly-budget"] / monthlyTotalHours
+
+            Profile.data = {
+                ...Profile.data,
+                ...req.body,
+                "value-hour": valueHour
+            }
+
+            return res.redirect('/profile')
+        }
+    }
 }
 
 const Job = {
@@ -40,7 +76,7 @@ const Job = {
                     ...job,
                     remaining,
                     status,
-                    budget: profile["value-hour"] * job["total-hours"]
+                    budget: Profile.data["value-hour"] * job["total-hours"]
                 }
             })
         
@@ -52,7 +88,7 @@ const Job = {
         save(req, res) {
             const lastId = Job.data[Job.data.length - 1]?.id || 1; // Verificando se ha algum elemento no array, caso nao, atribui id 1 ao primeiro elemento
      
-            jobs.push({
+            Job.data.push({
                 id: lastId + 1, // atribuindo + 1 no id dos proximos elementos
                 name: req.body.name,
                 "daily-hours": req.body["daily-hours"],
@@ -96,6 +132,7 @@ routes.post('/job', Job.controllers.save);
 
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"));
 
-routes.get('/profile', (req, res) => res.render(views + "profile", { profile }));
+routes.get('/profile', Profile.controllers.index);
+routes.post('/profile', Profile.controllers.update);
 
 module.exports = routes;
